f8ed04c304696b7d7fa2f29c7d6bd461a363e8ea
qnn4ik <qnn4ik@mail.ru>
2023-07-26 01:57:08 +0300

    added items

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,12 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from django.urls import path, include
 from core.views import *
+from item.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
-]
+    path('', include('core.urls')),
+    # get rid of the last line
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
