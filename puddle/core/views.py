d579dd1ab3efab31d4b9771f9fdf8b0c9542bffe
qnn4ik <qnn4ik@mail.ru>
2023-07-30 13:48:45 +0300

    update repo

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -4,7 +4,7 @@ from item.models import Category, Item
 
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+    items = Item.objects.filter(is_sold=False)
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
