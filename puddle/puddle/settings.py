f90eea68c720b895e60a19b18673a4b241ef8cfa
qnn4ik <qnn4ik@mail.ru>
2023-11-02 22:10:55 +0300

    changed DB to Postgres

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -90,9 +90,18 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'marketplace_db',
+        'USER': 'marketplace',
+        'PASSWORD': 'marketplace',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
